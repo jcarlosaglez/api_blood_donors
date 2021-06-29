f55c2a2251d4103d308f6d6eca742cd39777ea65
Juan Carlos Andres Gonzalez <jcarlosaglezzz@gmail.com>
2021-06-29 00:54:55 -0500

    [add-feature] Agregando inicio de sesión, autorización y subida de archivos para el donador.

--- controllers/Donor.js
+++ controllers/Donor.js
@@ -1,6 +1,8 @@
 const Donor = require('mongoose').model('Donor');
+const {unlink} = require('fs/promises');
+const passport = require('passport');
 
-async function d_create(req, res, next) {
+async function createDonor(req, res, next) {
     const body = req.body;
 
     if(!body.password) {
@@ -10,8 +12,25 @@ async function d_create(req, res, next) {
             data: {'password': 'El campo Contraseña es requerido.'}
         });
     }
-    console.log(typeof body.form_answers)
-    if(body.form_answers.constructor !== Object || (Object.keys(body.form_answers).length === 0 && body.form_answers.constructor === Object)) {
+
+    if(!body.certified_file) {
+        if(!req?.file?.filename) {
+            return res.status(400).json({
+                success: false,
+                msg: 'Algunos campos presentan un error de validación.',
+                data: {'certified_file': 'El campo Archivo certificado es requerido.'}
+            });
+        }
+    }
+
+    try {
+        body.form_answers = JSON.parse(body.form_answers);
+    }
+    catch {
+        console.log("No es un json");
+    }
+    
+    if(body.form_answers?.constructor !== Object || (Object.keys(body.form_answers).length === 0 && body.form_answers.constructor === Object)) {
         return res.status(400).json({
             success: false,
             msg: 'Algunos campos presentan un error de validación.',
@@ -23,6 +42,7 @@ async function d_create(req, res, next) {
 
     delete body.password;
     body.status = 'activo';
+    body.certified_file = req.file.filename;
 
     const donor = new Donor(body);
     donor.createPassword(password);
@@ -37,6 +57,8 @@ async function d_create(req, res, next) {
         });
     }
     catch (error) {
+        removeFile(`./public/certified_files/${body.certified_file}`);
+    
         if(error.name === 'ValidationError') {
             let validationErrors = {};
 
@@ -58,54 +80,282 @@ async function d_create(req, res, next) {
     }
 }
 
-function d_readOne(req, res, next) {
+async function readOneDonor(req, res, next) {
+    const id = req.params.id;
+    try {
+        const donor = await Donor.findById(id);
+
+        return res.status(200).json({
+            success: true,
+            data: donor.publicData()
+        });
+    }
+    catch (error) {
+        return res.status(404).json({
+            success: false,
+            msg: 'No se encontro a un Donador con el ID proporcionado.'
+        });
+    }
+}
+
+async function readAllDonors(req, res, next) {
+    try {
+        const donors = await Donor.find({});
+
+        return res.status(200).json({
+            success: true,
+            data: donors.map(donor => donor.publicData())
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            msg: 'Algo salio mal.'
+        });
+    }
+}
+
+async function updateDonor(req, res, next) {
+    if(req.params.id != req.user.id) {
+        return res.status(400).json({
+            success: false,
+            msg: 'Solo puedes editar tu propio perfil.'
+        });
+    }
+
+    const id = req.user.id;
+    const properties = [
+        "curp",
+        "first_name",
+        "last_name",
+        "date_of_birth",
+        "gender",
+        "email",
+        "phone_number",
+        "place_of_residence",
+        "blood_type",
+        "form_answers",
+        "status"
+    ];
+
+    if(Object.keys(req.body).length < 1) {
+        return res.status(400).json({
+            success: false,
+            msg: 'No hay cambios a efectuar.'
+        });
+    }
+
+    try {
+        const donor = await Donor.findById(id);
+        const body = req.body;
+
+        properties.forEach(property => {
+            donor[property] = body[property] ? body[property] : donor[property];
+        });
+
+        const updatedDonor = await donor.save();
+
+        return res.status(200).json({
+            success: true,
+            data: updatedDonor.publicData()
+        });
+    }
+    catch (error) {
+        if(error.name === 'ValidationError') {
+            let validationErrors = {};
+
+            Object.keys(error.errors).forEach(key => {
+                validationErrors[key] = error.errors[key].message
+            });
+
+            return res.status(400).json({
+                success: false,
+                msg: 'Algunos campos presentan un error de validación.',
+                data: validationErrors
+            });
+        }
+
+        return res.status(404).json({
+            success: false,
+            msg: 'No se encontro a un Donador con el ID proporcionado.'
+        });
+    }
+}
+
+async function deleteDonor(req, res, next) {
+    if(req.params.id != req.user.id) {
+        return res.status(400).json({
+            success: false,
+            msg: 'Solo puedes eliminar tu propio perfil.'
+        });
+    }
+
+    const id = req.user.id;
+
+    try {
+        const donor = await Donor.findById(id);
+
+        donor.status = 'eliminado';
+        
+        await donor.save();
+
+        return res.status(200).json({
+            success: true,
+            msg: 'Tu cuenta a sido eliminada.'
+        });
+    }
+    catch (error) {
+        return res.status(404).json({
+            success: false,
+            msg: 'No se encontro a un Donador con el ID proporcionado.'
+        });
+    }
+}
+
+async function login(req, res, next) {
+    const {email, password} = req.body;
+
+    if(!email || !password) {
+        return res.status(200).json({
+            success: false,
+            msg: "Se require del correo y contraseña."
+        });
+    }
+
+    passport.authenticate("local-donor", {session: false}, function(err, user, info) {
+        if(err) {
+            return next(err);
+        }
+        if(user) {
+            return res.status(200).json({
+                success: true,
+                data: user.toAuthJson()
+            });
+        }
+        else {
+            return res.status(400).json(info);
+        }
+    })(req, res, next);
+}
+
+async function search(req, res, next) {
+    const properties = [
+        "curp",
+        "place_of_residence",
+        "blood_type",
+        "status"
+    ];
+
+    try {
+        const body = req.body;
+        const search = {};
+
+        Object.keys(body).forEach(key => {
+            if(properties.includes(key)) {
+                search[key] = body[key];
+            }
+        });
+
+        if(Object.keys(search).length < 1) {
+            return res.status(400).json({
+                success: false,
+                msg: 'No hay parametros de busqueda.'
+            });
+        }
+
+        const donor = await Donor.find(search);
+        const donors = donor.map(donor => donor.publicData());
+
+        if(donors.length > 0) {
+            return res.status(200).json({
+                success: true,
+                data: donors
+            });
+        }
+        else {
+            return res.status(404).json({
+                success: false
+            });
+        }
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            msg: 'Algo salio mal.'
+        });
+    }
+}
+
+async function changeCertifiedFile(req, res, next) {
+    if(req.params.id != req.user.id) {
+        return res.status(400).json({
+            success: false,
+            msg: 'Solo puedes cambiar tu propio archivo certificado.'
+        });
+    }
+
+    const id = req.user.id;
+
+    if(!req.body.certified_file) {
+        if(!req?.file?.filename) {
+            return res.status(400).json({
+                success: false,
+                msg: 'Algunos campos presentan un error de validación.',
+                data: {'certified_file': 'El campo Archivo certificado es requerido.'}
+            });
+        }
+    }
+
+    try {
+        const donor = await Donor.findById(id);
+
+        removeFile(`./public/certified_files/${donor.certified_file}`);
+
+        donor.certified_file = req.file.filename;
+
+        const updatedDonor = await donor.save();
+
+        return res.status(200).json({
+            success: true,
+            data: updatedDonor.publicData()
+        });
+    }
+    catch (error) {
+        removeFile(`./public/certified_files/${req.file.filename}`);
+
+        return res.status(404).json({
+            success: false,
+            msg: 'No se encontro a un Donador con el ID proporcionado.'
+        });
+    }
+}
+
+async function me(req, res, next) {
+    const donor = await Donor.findById(req.user.id);
+
     return res.status(200).json({
         success: true,
-        msg: "TODO Read one"
+        data: donor.fullData()
     });
 }
 
-function d_readAll(req, res, next) {
-    return res.status(200).json({
-        success: true,
-        msg: "TODO Read all"
-    });
-}
-
-function d_update(req, res, next) {
-    return res.status(200).json({
-        success: true,
-        msg: "TODO Update"
-    });
-}
-
-function d_delete(req, res, next) {
-    return res.status(200).json({
-        success: true,
-        msg: "TODO Delete"
-    });
-}
-
-function d_login(req, res, next) {
-    return res.status(200).json({
-        success: true,
-        msg: "TODO Login"
-    });
-}
-
-function d_search(req, res, next) {
-    return res.status(200).json({
-        success: true,
-        msg: "TODO Search"
-    });
+async function removeFile(path) {
+    try {
+        await unlink(path);
+    }
+    catch (error) {
+        console.log('El archivo no se pudo eliminar.');
+    }
 }
 
 module.exports = {
-    d_create,
-    d_readOne,
-    d_readAll,
-    d_update,
-    d_delete,
-    d_login,
-    d_search
+    createDonor,
+    readOneDonor,
+    readAllDonors,
+    updateDonor,
+    deleteDonor,
+    login,
+    search,
+    changeCertifiedFile,
+    me
 }
--- routes/Donor.js
+++ routes/Donor.js
@@ -1,20 +1,79 @@
 const router = require('express').Router();
+const storage = require('../config/multer.js');
+const auth = require('./auth.js');
+const is_donor = require('../middlewares/is_donor.js')
 const {
-    d_create,
-    d_readOne,
-    d_readAll,
-    d_update,
-    d_delete,
-    d_login,
-    d_search
+    createDonor,
+    readOneDonor,
+    readAllDonors,
+    updateDonor,
+    deleteDonor,
+    login,
+    search,
+    changeCertifiedFile,
+    me
 } = require('../controllers/Donor.js');
 
-router.post('/', d_create);
-router.get('/:id', d_readOne);
-router.get('/', d_readAll);
-router.put('/:id', d_update);
-router.delete('/:id', d_delete);
-router.post('/login', d_login);
-router.get('/search', d_search);
+router.post('/login', login);
+router.post('/', storage.single('certified_file'), createDonor, (error, req, res, next) => {
+    if(error.code === 'LIMIT_FILE_SIZE') {
+        error.message = 'El archivo a subir supera el limite de 1MB.';
+    }
+
+    return res.status(400).json({
+        success: false,
+        msg: 'Algunos campos presentan un error de validación.',
+        data: {'certified_file': error.message}
+    });
+});
+router.get('/me', [auth.required, is_donor], me, (error, req, res, next) => {
+    if(error.name === 'UnauthorizedError') {
+        return res.status(400).json({
+            success: false,
+            msg: 'Error de autorización.',
+            data: {'code': error.code, 'message': error.message}
+        });
+    }
+});
+router.get('/search', search);
+router.get('/:id', readOneDonor);
+router.get('/',     readAllDonors);
+router.patch('/:id', [auth.required, is_donor], updateDonor, (error, req, res, next) => {
+    if(error.name === 'UnauthorizedError') {
+        return res.status(400).json({
+            success: false,
+            msg: 'Error de autorización.',
+            data: {'code': error.code, 'message': error.message}
+        });
+    }
+});
+router.patch('/:id/change_certified_file', [auth.required, is_donor, storage.single('certified_file')], changeCertifiedFile, (error, req, res, next) => {
+    if(error.name === 'UnauthorizedError') {
+        return res.status(400).json({
+            success: false,
+            msg: 'Error de autorización.',
+            data: {'code': error.code, 'message': error.message}
+        });
+    }
+    
+    if(error.code === 'LIMIT_FILE_SIZE') {
+        error.message = 'El archivo a subir supera el limite de 1MB.';
+    }
+
+    return res.status(400).json({
+        success: false,
+        msg: 'Algunos campos presentan un error de validación.',
+        data: {'certified_file': error.message}
+    });
+});
+router.delete('/:id', [auth.required, is_donor], deleteDonor, (error, req, res, next) => {
+    if(error.name === 'UnauthorizedError') {
+        return res.status(400).json({
+            success: false,
+            msg: 'Error de autorización.',
+            data: {'code': error.code, 'message': error.message}
+        });
+    }
+});
 
 module.exports = router;
